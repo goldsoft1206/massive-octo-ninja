09483a4bd8cc20b2f46fa1fa1f665876dbf0de7c
goldsoft1206 <icbsoft87@gmail.com>
2014-11-05 21:47:37 +0800

    picture display in media url

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
@@ -14,4 +16,6 @@ urlpatterns = patterns('',
 
     url(r'^accounts/', include('allauth.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
